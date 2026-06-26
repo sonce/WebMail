929f3d4430a2a0a536178af238e480608f0a4a6e
ASheng <asheng@local>
2026-06-26 14:13:57 +0800

    feat: register services and auth policies

--- src/WebMail/Program.cs
+++ src/WebMail/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using WebMail.Data;
+using WebMail.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<WebMailDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<BuyerRuleService>();
+builder.Services.AddSingleton<CardGenerationService>();
+builder.Services.AddSingleton<MailSyncPlanner>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => { options.LoginPath = "/Login"; options.AccessDeniedPath = "/AccessDenied"; });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Administrator"));
+    options.AddPolicy("SalesOnly", policy => policy.RequireRole("Sales"));
+    options.AddPolicy("SupplierOnly", policy => policy.RequireRole("Supplier"));
+});
 
 var app = builder.Build();
 
@@ -23,6 +36,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
